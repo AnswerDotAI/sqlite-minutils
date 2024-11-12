da0221de50dcb9ef1d5b19c4724bae15901e6670
Audrey M. Roy Greenfeld <arg@answer.ai>
2024-11-12 16:57:20 +0000

    Tests for updates where nothing is updated
    Co-authored-by: Daniel Roy Greenfeld <drg@answer.ai>

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,6 +1,8 @@
 import types
 
 
+# Basic query tests
+
 def test_query(fresh_db):
     fresh_db["dogs"].insert_all([{"name": "Cleo"}, {"name": "Pancakes"}])
     results = fresh_db.query("select * from dogs order by name desc")
@@ -15,3 +17,11 @@ def test_execute_returning_dicts(fresh_db):
     assert fresh_db.execute_returning_dicts("select * from test") == [
         {"id": 1, "bar": 2}
     ]
+
+def test_query_no_update(fresh_db):
+    """Test that a query that doesn't update the database:
+    1) Returns an empty list and 2) Doesn't change the database"""
+    fresh_db["message"].insert({"msg_type": "greeting", "content": "hello"})
+    results = fresh_db.query("update message set msg_type='note' where msg_type='md'")
+    assert list(results) == []
+    assert list(fresh_db["message"].rows) == [{"msg_type": "greeting", "content": "hello"}]
